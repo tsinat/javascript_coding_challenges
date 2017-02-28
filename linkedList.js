9ae3c2ce669c365d1e2e14eb6a9d4350573f0ebb
Tsinat Zeree <cheru87@gmail.com>
2017-02-28 15:36:07 -0800

    basic implementation of Linked list in js

---
+++ linkedList.js
@@ -0,0 +1,45 @@
+// javascript implementation of LinkedList
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, previous) {
+  this.value = value;
+  this.next = next;
+  this.prev = previous;
+
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value, this.head, null);
+  if(this.head) this.head.prev = newNode;
+  else this.tail = newNode;
+  this.head = newNode;
+}
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail);
+  if(this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  this.tail = newNode;
+}
+
+LinkedList.prototype.removeHead = function() {
+  if(!this.head) return null;
+  var val = this.head.value;
+  this.head = this.head.next;
+  if(this.head) this.head.prev = null;
+  else this.tail = null;
+  return val;
+}
+
+
+var ll = new LinkedList();
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(300);
+ll.addToTail(400);
+
+
+console.log(ll.removeHead());
