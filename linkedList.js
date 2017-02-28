28248578b73faaff76baad613d99b8382a36940d
Tsinat Zeree <cheru87@gmail.com>
2017-02-28 15:42:24 -0800

    removing tail from linked list

--- linkedList.js
+++ linkedList.js
@@ -34,6 +34,14 @@ LinkedList.prototype.removeHead = function() {
   return val;
 }
 
+LinkedList.prototype.removeTail = function() {
+  if(!this.tail) return null;
+  var val = this.tail;
+  this.tail = this.tail.prev
+  if(this.tail) this.tail.next = null;
+  else this.head = null;
+  return val;
+}
 
 var ll = new LinkedList();
 ll.addToHead(100);
