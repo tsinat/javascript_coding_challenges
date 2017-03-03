9bc2a4484323ef9a13eb26c1f886a9b350b9a4eb
Tsinat Zeree <cheru87@gmail.com>
2017-03-02 18:43:31 -0800

    hash table insert and get methods completed

--- hashTable.js
+++ hashTable.js
@@ -5,11 +5,60 @@ function HashTable(size) {
     this.numBucket = this.buckets.length;
 }
 
-function HashNode = function(key, value, next) {
+function HashNode(key, value, next) {
     this.key = key;
     this.value = value;
     this.next = next || null;
 }
 
+HashTable.prototype.hash = function(key) {
+    var total = 0;
+    for(var i = 0; i < key.length; i++) {
+        total += key.charCodeAt(i);
+    }
+    var bucket = total % this.numBucket;
+    return bucket;
+}
+
+HashTable.prototype.insert = function(key, value) {
+    var index = this.hash(key);
+    if(!this.buckets[index]) {
+        this.buckets[index] = new HashNode(key, value);
+    }
+    else if(this.buckets[index].key === key) {
+        this.buckets[index].value = value
+    }
+    else {
+        var currentNode = this.buckets[index];
+        while(currentNode.next) {
+            if(currentNode.next.key === key) {
+                currentNode.next.value = value;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+        currentNode.next = new HashNode(key, value);
+    }
+}
+
+HashTable.prototype.get = function(key) {
+    var index = this.hash(key);
+    if(!this.buckets[index]) {
+        return null;
+    }
+    else {
+        var currentNode = this.buckets[index];
+        while(currentNode) {
+            if(currentNode.key === key) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+}
+
 var myHT = new HashTable(30);
-console.log(myHT);
+myHT.insert('tsinat', 'cheru87@gmail.com')
+myHT.insert('tsinat', 'tsinatzeree@gmail.com')
+console.log(myHT.get('tsinat'));
