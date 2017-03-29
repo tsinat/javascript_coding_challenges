1ff2e42692b7817415b4d69e778c6c3949ba969c
Tsinat Zeree <cheru87@gmail.com>
2017-03-29 15:44:39 -0700

    stock exchange challenge

---
+++ stockExchange.js
@@ -0,0 +1,12 @@
+//  the maximum profit one could get by selling once and buying once in a day
+//
+
+function bestSale(arr) {
+    var profite = arr[0],
+        currentProfite = - 10000,
+        grandProfile = arr[0];
+
+    for(var i = 0; i < arr.length; i++) {
+
+    }
+}
